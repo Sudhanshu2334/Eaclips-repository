70a4e854ba83f2245445de246f1f9605d0baec50
Sudhanshu2334 <sk3415475@gmail.com>
2022-06-01 17:02:17 +0530

    This is my second code.

--- TestGitHub/src/com/demo/TestClass.java
+++ TestGitHub/src/com/demo/TestClass.java
@@ -5,7 +5,7 @@ public class TestClass {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("This is my first github project");
-
+		System.out.println("this is my second project");
 	}
 
 }
